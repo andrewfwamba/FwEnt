ed84b7313c3cb53245aac896aaf7b0b4ba6041c1
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-15 16:02:06 +0300

    update configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,18 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const SERVER_PORT = process.env.SERVER_PORT
+  ? Number(process.env.SERVER_PORT)
+  : 4000;
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@nodedb.n1jqsqd.mongodb.net/fworks`;
+export const config = {
+  mongo: {
+    url: MONGO_URL,
+  },
+  server: {
+    port: SERVER_PORT,
+  },
+};
