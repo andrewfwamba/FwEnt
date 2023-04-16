eddfc5463da46177dcdde459d0cc5a163f993f97
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-16 23:30:31 +0300

    music model

---
+++ src/models/Music.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import { Document } from "mongoose";
+export interface IMusic {
+  title: string;
+  artist: string;
+}
+
+export interface IMusicModel extends IMusic, Document {}
+
+const MusicSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true },
+    artist: { type: Schema.Types.ObjectId, required: true, ref: "Artist" },
+  },
+  {
+    timestamps: true,
+    versionKey: true,
+  }
+);
+
+export default mongoose.model<IMusicModel>("Music", MusicSchema);
