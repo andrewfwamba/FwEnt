ff1d0d835f917e35b2d2f7e8ae06d0f682c59a5b
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-17 15:10:24 +0300

    middleware

--- src/routes/Artist.ts
+++ src/routes/Artist.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import controller from "../controllers/Artist";
+import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
 const router = express.Router();
 
-router.post("/create", controller.createArtist);
+router.post(
+  "/create",
+  ValidateSchema(Schemas.artist.create),
+  controller.createArtist
+);
 router.get("/get/:artistId", controller.getArtist);
 router.get("/get", controller.getAllArtist);
-router.patch("/update/:artistId", controller.updateArtist);
+router.patch(
+  "/update/:artistId",
+  ValidateSchema(Schemas.artist.update),
+  controller.updateArtist
+);
 router.delete("/delete/:artistId", controller.deleteArtist);
 
 export = router;
