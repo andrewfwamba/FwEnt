22aeb9a55add0b6bb6540735d4e956a027218a55
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-19 13:34:08 +0300

    fix model err

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Model, Schema, model } from "mongoose";
 import * as bcrypt from "bcrypt";
 
 export interface IUser {
@@ -9,7 +9,13 @@ export interface IUser {
   token: string;
 }
 
-export interface IUserModel extends IUser, Document {}
+export interface IUserModel extends IUser, Document {
+  comparePassword(password: string): Promise<boolean>;
+}
+
+export interface IUserModelStatic extends Model<IUserModel> {
+  isThisEmailInUse(email: string): Promise<boolean>;
+}
 
 const UserSchema: Schema = new mongoose.Schema(
   {
@@ -24,27 +30,34 @@ const UserSchema: Schema = new mongoose.Schema(
   }
 );
 
-UserSchema.pre("save", function (next) {
+UserSchema.pre<IUserModel>("save", async function (next) {
   if (this.isModified("password")) {
-    bcrypt.hash(this.password, 8, (err, hash) => {
-      if (err) return next(err);
-
+    try {
+      const salt = await bcrypt.genSalt(8);
+      const hash = await bcrypt.hash(this.password, salt);
       this.password = hash;
       next();
-    });
+    } catch (err: any) {
+      return next(err);
+    }
   }
 });
 
-UserSchema.methods.comparePassword = async function (password: string) {
+UserSchema.methods.comparePassword = async function (
+  this: IUserModel,
+  password: string
+) {
   if (!password) throw new Error("Missing password, cannot compare. ");
   try {
     const result = await bcrypt.compare(password, this.password);
     return result;
   } catch (error: any) {
     console.log("Error while trying to compare password", error.message);
+    return false;
   }
 };
-UserSchema.statics.isThisEmailInUse = async function (email) {
+
+UserSchema.statics.isThisEmailInUse = async function (email: string) {
   if (!email) throw new Error("Invalid email provided");
   try {
     const user = await this.findOne({ email });
@@ -57,4 +70,30 @@ UserSchema.statics.isThisEmailInUse = async function (email) {
   }
 };
 
-export default mongoose.model<IUserModel>("User", UserSchema);
+// UserSchema.statics.isThisEmailInUse = async function (
+//   this: IUserModelStatic,
+//   email: string
+// ) {
+//   if (!email) throw new Error("Invalid email provided");
+//   try {
+//     const user = await this.findOne({ email });
+//     if (user) return false;
+
+//     return true;
+//   } catch (error: any) {
+//     console.log("error in the method", error.message);
+//     return false;
+//   }
+// };
+
+// const UserModel: IUserModelStatic = mongoose.model<
+//   IUserModel,
+//   IUserModelStatic
+// >("User", UserSchema);
+
+// export default UserModel;
+const UserModel: IUserModelStatic = <IUserModelStatic>(
+  model<IUserModel>("User", UserSchema)
+);
+
+export default UserModel;
