07991c7aedb98a6809fb1a887b78fa5193b2aeca
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-20 19:16:16 +0300

    protect route

--- src/routes/Artist.ts
+++ src/routes/Artist.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import controller from "../controllers/Artist";
 import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
+import { isAuth } from "../middleware/auth";
 const router = express.Router();
 
 router.post(
   "/create",
   ValidateSchema(Schemas.artist.create),
+  // isAuth as (req: Request & AuthenticatedRequest, res: Response, next: NextFunction) => Promise<void>,
   controller.createArtist
 );
-router.get("/get/:artistId", controller.getArtist);
-router.get("/get", controller.getAllArtist);
+router.get("/get/:artistId", isAuth, controller.getArtist);
+router.get("/get/", controller.getAllArtist);
 router.patch(
   "/update/:artistId",
   ValidateSchema(Schemas.artist.update),
