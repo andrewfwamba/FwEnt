f5f0228d8a9c2e0d4bbf0c7af74c0be4ea36e9b4
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-21 22:53:48 +0300

    cleanup

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -3,15 +3,15 @@ import controller from "../controllers/User";
 import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
 const router = express.Router();
 
-router.post("/create", controller.createUser);
-router.post("/signin", controller.loginUser);
-// router.get("/get/:musicId", controller.getMusic);
-// router.get("/get", controller.getAllMusic);
-// router.patch(
-//   "/update/:musicId",
-//   ValidateSchema(Schemas.music.update),
-//   controller.updateMusic
-// );
-// router.delete("/delete/:musicId", controller.deleteMusic);
+router.post(
+  "/create",
+  ValidateSchema(Schemas.user.create),
+  controller.createUser
+);
+router.post(
+  "/signin",
+  ValidateSchema(Schemas.user.signin),
+  controller.loginUser
+);
 
 export = router;
