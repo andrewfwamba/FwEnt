894eead7af8622e4da221c9a94f0bf5ba2740467
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-21 22:52:02 +0300

    fix middleware bug

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,29 +3,22 @@ import User, { IUserModel } from "../models/User";
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
 const seckey = process.env.JWT_SECRET || "";
-// interface AuthenticatedRequest extends Request {
-//   user: IUserModel;
-// }
-
-export const isAuth = async (
+export async function isAuthenticated(
   req: Request,
   res: Response,
   next: NextFunction
-) => {
-  // Get the token from the request headers
+) {
   if (req.headers && req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
-
     try {
+      // check if the user exists
       const decode = jwt.verify(token, seckey) as { userId: string };
       const user = await User.findById(decode.userId);
-
       if (!user) {
         return res
           .status(401)
           .json({ success: false, message: "Unauthorized access" });
       }
-
       // Check if the user is the current authenticated user
       // if (user._id.toString() !== req.params.userId) {
       //   return res.status(403).json({ message: "Forbidden" });
@@ -33,20 +26,25 @@ export const isAuth = async (
 
       console.log(user._id);
       // Add the user object to the request object for later use
-      // req.user = user;
+      const userInfo = {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+      };
       next();
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
+      // check token validity
       if (error.name === "JsonWebTokenError") {
         return res.json({ success: false, message: "Invalid Access token" });
       }
+      // catch token expired error
       if (error.name === "TokenExpiredError") {
         return res.json({
           success: false,
           message: "Session expired try sign in",
         });
       }
-
       res
         .status(500)
         .json({ success: false, message: "Internal server error" });
@@ -54,6 +52,51 @@ export const isAuth = async (
   } else {
     res.json({ success: false, message: "Unauthorized access" });
   }
-};
+}
+
+// export function isAuth(req: Request, res: Response, next: NextFunction) => {
+//   // Get the token from the request headers
+//   if (req.headers && req.headers.authorization) {
+//     const token = req.headers.authorization.split(" ")[1];
+
+//     try {
+//       const decode = jwt.verify(token, seckey) as { userId: string };
+//       const user = await User.findById(decode.userId);
+
+//       if (!user) {
+//         return res
+//           .status(401)
+//           .json({ success: false, message: "Unauthorized access" });
+//       }
+
+//       // Check if the user is the current authenticated user
+//       if (user._id.toString() !== req.params.userId) {
+//         return res.status(403).json({ message: "Forbidden" });
+//       }
+
+//       console.log(user._id);
+//       // Add the user object to the request object for later use
+//       // req.user = user;
+//       next();
+//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//     } catch (error: any) {
+//       if (error.name === "JsonWebTokenError") {
+//         return res.json({ success: false, message: "Invalid Access token" });
+//       }
+//       if (error.name === "TokenExpiredError") {
+//         return res.json({
+//           success: false,
+//           message: "Session expired try sign in",
+//         });
+//       }
+
+//       res
+//         .status(500)
+//         .json({ success: false, message: "Internal server error" });
+//     }
+//   } else {
+//     res.json({ success: false, message: "Unauthorized access" });
+//   }
+// };
 
 // export default { isAuth };
