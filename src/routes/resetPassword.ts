5be6bb68d32981ecb6371e36802ebc04d60a80b1
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-23 01:25:07 +0300

    restructure mail format

--- src/routes/resetPassword.ts
+++ src/routes/resetPassword.ts
@@ -30,9 +30,32 @@ router.post(
         }).save();
       }
       const otp = token.token.toString();
-      const subject = "Reset Password OTP";
-      const body = `<div style="background-color: #ACB6F0; border-radius: 5px;"><h4 style="font-size: 20px; margin: 3px; color: #141A3D;">Greetings from the Fireworks team</br></h4><p style="font-size: 20px;">Your password reset OTP is: ${otp}</p></div>`;
-      await sendEmail(user.email, subject, body);
+      const button =
+        ' <a href="" style="background-color:#E0E3F0; border:1px solid #ffbe00; border-color:#152363; border-radius:5px; border-width:1px; color:#000000; display:inline-block; font-size:14px; font-weight:normal; letter-spacing:0px; line-height:normal; padding:12px 40px 12px 40px; text-align:center; text-decoration:none; border-style:solid; font-family:inherit;" target="_blank">Verify Email Now</a>';
+      const subject = `Password reset`;
+      const body = `<span style="font-size: 15px">Use this OTP to reset your account's password </span>
+                    <span style="color: #000000; font-size: 15px; font-family: arial, helvetica, sans-serif">to regain access </span>
+                    <span style="font-size: 18px">.</span>
+                    <div style="font-family: inherit; text-align: left"><span style="color: #152363; font-size: 18px">
+                    <strong>OTP: ${otp}</strong>
+                    <p style="font-size: 14px">Expires in 1hour</p></span>
+                    </div>`;
+      const info = `  <tbody>
+                    <tr>
+                      <td style="padding:40px 20px 40px 20px; line-height:18px; text-align:inherit; background-color:#6e6e6e;" height="100%" valign="top" bgcolor="#6e6e6e" role="module-content"><div><div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 15px"><strong>You may follow these simple steps:</strong></span></div>
+                      <div style="font-family: inherit; text-align: center"><br></div>
+                      <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">1. Enter the otp on the OTP field on our app for verification.</span></div>
+                      <div style="font-family: inherit; text-align: center"><br></div>
+                      <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">2. After verification is complete you'll be prompted to input a new password.</span></div>
+                      <div style="font-family: inherit; text-align: center"><br></div>
+                      <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">3. Provide a secure password that you can easily remember.</span></div>
+                      <div style="font-family: inherit; text-align: center"><span style="color: #152363; font-size: 13px"><strong>Congrats!! You've successfully reset your password </strong></span></div>
+                      <div style="font-family: inherit; text-align: center"><br></div>
+                      <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">Need support? Our support team is always</span></div>
+                      <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">ready to help!</span></div><div></div></div></td>
+                    </tr>
+                  </tbody>`;
+      await sendEmail(user.email, user.name, subject, body, info);
 
       res.send({
         success: true,
