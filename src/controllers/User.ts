b32965422415acb61e0b3a17a2c0607328ceacd9
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-25 23:38:29 +0300

    integrated signup email

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,6 +1,7 @@
 import * as jwt from "jsonwebtoken";
 import { Request, Response } from "express";
 import User, { IUserModel, IUserModelStatic } from "../models/User";
+import { sendEmail } from "../utilities/sendEmail";
 
 const createUser = async (req: Request, res: Response) => {
   const { name, phone, email, password, confirmpassword } = req.body;
@@ -19,6 +20,31 @@ const createUser = async (req: Request, res: Response) => {
     phone: user.phone,
     email: user.email,
   };
+  const subject = `Welcome to Fireworks Entertainment`;
+  const body = `<span style="font-size: 15px">Thank you for signing up with us.  Be sure to be entertained by the best. </span>
+  <span style="color: #000000; font-size: 15px; font-family: arial, helvetica, sans-serif"> </span>
+  <span style="font-size: 18px">.</span>
+  <div style="font-family: inherit; text-align: left"><span style="color: #152363; font-size: 18px">
+  <strong>You'll never get enough 😉😜</strong>
+  <p style="font-size: 14px"></p></span>
+  </div>`;
+  const greeting = `Welcome, ${userinfo.name}`;
+  const info = `  <tbody>
+  <tr>
+    <td style="padding:40px 20px 40px 20px; line-height:18px; text-align:inherit; background-color:#6e6e6e;" height="100%" valign="top" bgcolor="#6e6e6e" role="module-content"><div><div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 15px"><strong>Welcome to Fireworks entertainment</strong></span></div>
+    <div style="font-family: inherit; text-align: center"><br></div>
+    <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">Enjoy Hot mixes from our super djs.</span></div>
+    <div style="font-family: inherit; text-align: center"><br></div>
+    <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">For all your music tastes</span></div>
+    <div style="font-family: inherit; text-align: center"><br></div>
+    <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">From locals to international</span></div>
+    <div style="font-family: inherit; text-align: center"><span style="color: #152363; font-size: 13px"><strong>Enjoy!!</strong></span></div>
+    <div style="font-family: inherit; text-align: center"><br></div>
+    <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">Need support? Our support team is always</span></div>
+    <div style="font-family: inherit; text-align: center"><span style="color: #ffffff; font-size: 13px">ready to help!</span></div><div></div></div></td>
+  </tr>
+</tbody>`;
+  sendEmail(userinfo.email, greeting, subject, body, info);
 
   res.json({ success: true, userinfo });
 };
