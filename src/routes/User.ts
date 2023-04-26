2eb377279eb0193a6181ac8eafc2016a4a2295c3
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-26 22:18:41 +0300

    conf upload profile route

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,7 +1,22 @@
 import express from "express";
 import controller from "../controllers/User";
 import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
+import multer from "multer";
+import { isAuthenticated } from "../middleware/auth";
 const router = express.Router();
+
+const storage = multer.diskStorage({});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const fileFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb("invalid image type!", false);
+  }
+};
+
+const uploads = multer({ storage, fileFilter });
 
 router.post(
   "/create",
@@ -12,6 +27,13 @@ router.post(
   "/signin",
   ValidateSchema(Schemas.user.signin),
   controller.loginUser
+);
+
+router.post(
+  "/upload-profile",
+  isAuthenticated,
+  uploads.single("profile"),
+  controller.uploadProfile
 );
 
 export = router;
