e45971d98bb02542500e2b0c01b48a928c5be05b
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-04-26 22:19:08 +0300

    fixed cloudinary config

--- src/utilities/cloudinary.ts
+++ src/utilities/cloudinary.ts
@@ -1,9 +1,9 @@
 import * as cloudinary from "cloudinary";
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_USERNAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_USERNAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-// export = cloudinary;
+export = cloudinary;
