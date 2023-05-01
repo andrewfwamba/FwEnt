5d9fb723674e9d1e6f0cb9ddc8c0f87970d696b9
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-01 22:50:42 +0300

    added streaming route

--- src/controllers/Music.ts
+++ src/controllers/Music.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 import Music from "../models/Music";
 import { Request, Response } from "express";
+import path from "path";
+import * as fs from "fs";
+import Logging from "../library/Logging";
 
-const createMusic = (req: Request, res: Response) => {
+const createMusic = (req: Request & { file?: any }, res: Response) => {
   const { title, artist } = req.body;
+  // Get file details from req.file
+  const url = path.join(req.file.destination, req.file.filename);
+
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
   const music = new Music({
     _id: new mongoose.Types.ObjectId(),
     title,
+    url,
     artist,
   });
   return music
@@ -60,10 +70,58 @@ const deleteMusic = (req: Request, res: Response) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+const stream = async (req: Request, res: Response) => {
+  try {
+    const title = req.params.title;
+    const music = await Music.findOne({ title });
+    if (!music) {
+      return res.status(404).json({ message: "File not found" });
+    }
+    // const filePathv = path.join(__dirname, 'mp3', title);
+    const filePath = path.join(__dirname, "..", music.url);
+
+    const stat = fs.statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      // Streaming support
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunksize = end - start + 1;
+      const file = fs.createReadStream(filePath, { start, end });
+      const head = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "audio/mpeg",
+      };
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      // Full file download
+      const head = {
+        "Content-Length": fileSize,
+        "Content-Type": "audio/mpeg",
+      };
+      res.writeHead(200, head);
+      res.on("error", (error) => {
+        Logging.error(error);
+      });
+      fs.createReadStream(filePath).pipe(res);
+    }
+  } catch (error) {
+    Logging.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 export default {
   createMusic,
   getMusic,
   getAllMusic,
   updateMusic,
   deleteMusic,
+  stream,
 };
