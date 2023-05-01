879277fd16afd1a8553eaf7a5c6e541e467bffd3
Andrew Fwamba <andrewecorrex03@gmail.com>
2023-05-01 22:51:03 +0300

    added streaming route endpoint

--- src/routes/Music.ts
+++ src/routes/Music.ts
@@ -1,10 +1,35 @@
 import express from "express";
 import controller from "../controllers/Music";
 import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
+import multer from "multer";
+import path from "path";
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    const source = process.env.BASE_URL || "";
+    const uniqueSuffix =
+      source + Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "audio/mpeg") {
+      return cb(new Error("Only mp3 files are allowed"));
+    }
+    cb(null, true);
+  },
+});
 
 router.post(
   "/create",
+  upload.single("music"),
   ValidateSchema(Schemas.music.create),
   controller.createMusic
 );
@@ -16,5 +41,7 @@ router.patch(
   controller.updateMusic
 );
 router.delete("/delete/:musicId", controller.deleteMusic);
+
+router.get("/mp3/:title", controller.stream);
 
 export = router;
